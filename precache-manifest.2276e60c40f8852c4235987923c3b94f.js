fd31f7dea931a71e8f8f52fc7a28f9e69458b996
Yong Lee <yongslee79@hotmail.com>
2019-09-13 23:47:59 -0400

    Updates

---
+++ precache-manifest.2276e60c40f8852c4235987923c3b94f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86e2802eb6b1fc7a6fa84bc8dba523e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1022cda19e0db00b6133",
+    "url": "/static/css/main.a84ceb36.chunk.css"
+  },
+  {
+    "revision": "a07ff31286d1df2e0a8a",
+    "url": "/static/js/2.aee2ac52.chunk.js"
+  },
+  {
+    "revision": "1022cda19e0db00b6133",
+    "url": "/static/js/main.d5d92364.chunk.js"
+  },
+  {
+    "revision": "eb7eba97ff0b1e2176a5",
+    "url": "/static/js/runtime~main.10deba10.js"
+  }
+]);
